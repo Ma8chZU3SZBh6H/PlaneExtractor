9ed7206ed27a54731c647b6636f3fd676f14e091
skindervik <38836685+skindervik@users.noreply.github.com>
2022-04-23 14:25:11 +0300

    get json with HttpClient

--- App/Program.cs
+++ App/Program.cs
@@ -2,6 +2,10 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        HttpClient client = new HttpClient();
+        HttpResponseMessage response = client.GetAsync("http://homeworktask.infare.lt/search.php?from=MAD&to=FUE&depart=2022-05-09&return=2022-05-16").Result;
+        HttpContent content = response.Content;
+        string result = content.ReadAsStringAsync().Result;
+        Console.WriteLine(result);
     }
 }
